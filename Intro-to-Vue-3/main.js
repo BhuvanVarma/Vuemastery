34e007870a00553e6e06460285d149ef3b92505a
BhuvanVarma <bhuvan.varma123@gmail.com>
2025-01-29 17:53:11 +0530

    completed

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
     data() {
-        return{
-            product: "Socks",
-            description: "This is green socks"
+        return {
+            cart: [],
+            premium: true
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
-})
-
-app.mount('#app')
+  })
+  
